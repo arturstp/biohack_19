fc74851ef45920517fb0f9bfb2abe3d50146c307
Viktor Beskorovainyi <211640@edu.p.lodz.pl>
2019-06-08 09:41:49 +0200

    data added

--- prepear_data.py
+++ prepear_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def main():
-    all_data = pd.read_csv('~/biohack/data/train.tsv', sep='\t')
+    all_data = pd.read_csv('./data/train.tsv', sep='\t')
     print(all_data)
 
 
