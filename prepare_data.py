9ad84783e91bdb36bea8f501563cc1eef4987658
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-06-09 08:04:15 +0200

    finish him

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 import sys
 from sklearn.metrics import auc
 from sklearn.metrics import roc_curve
@@ -48,7 +49,7 @@ char_to_num = {
 }
 
 mhc_to_num = {
-    'H2-Dd': 1, #HOW TO INTERPRET THIS?
+    'H2-Dd': 1,
     'H2-Db': 2,
     'H2-Kd': 3,
     'H2-Kb': 4,
@@ -57,7 +58,7 @@ mhc_to_num = {
 }
 
 
-def convertToNum(row):
+def convertToNum(row, is_test_data=False):
     peptide_in_num = np.empty((0, 10))
     peptide_in_num.ravel()
     text = row.loc['Peptide']
@@ -68,7 +69,8 @@ def convertToNum(row):
         b = np.append(b, mhc_to_num[row.loc['MHC']])
         peptide_in_num = np.vstack((peptide_in_num, b))
         i += 1
-
+    if is_test_data:
+        return peptide_in_num, np.empty(1)
     return peptide_in_num, row.loc['Immunogenicity']
 
 def read_data(path):
@@ -82,7 +84,7 @@ def add_zeros(data):
             text = text[:len(text)//2] + 'X' + text[len(text)//2:]
         data.at[index, 'Peptide'] = text
 
-def get_inputs(filename):
+def get_inputs(filename, is_test_data=False):
     all_data = read_data(filename)
 
     add_zeros(all_data)
@@ -90,14 +92,9 @@ def get_inputs(filename):
     acids_data = np.empty([all_data.shape[0], 11, 10])
     react_data = np.empty((all_data.shape[0], 1))
     for index, row in all_data.iterrows():
-        # acids_data[index], mhc_data[index], react_data[index] = convertToNum(row)
-        acids_data[index], react_data[index] = convertToNum(row)
-        # print(numbers)
+        acids_data[index], react_data[index] = convertToNum(row, is_test_data)
 
-        # data_new[index] = numbers
     return acids_data, react_data
-    # return acids_data, mhc_data, react_data
-
 
 def create_model():
 
@@ -130,24 +127,17 @@ def split_acids_mhc(data):
     x_mhc = data[:, -1, 9:]
     return x_main, x_mhc
 
-def main(argv):
-    EPOCHS = 20
-    SPLITS = 10
-    # data, mhc, react = get_inputs()  #todo: pass filename
-    train_set_path = './data/train.tsv'
-    data, react = get_inputs(train_set_path)
-    X = data
-    # X1 = data
-    # X2 = mhc
-    Y = react
 
+# sorry for those :all_auc,fpr_keras, tpr_keras
+# they are for gathering data to display later.
+# didn't finish refactoring
+def train_model(EPOCHS, SPLITS, X, Y, all_auc):
     skf = StratifiedKFold(n_splits=SPLITS, shuffle=True)
-    auc_best = 0.0
-    history_best = None
+
     model_best = None
     fpr_keras = None
     tpr_keras = None
-    all_auc = []
+    auc_best = 0.0
     for index, (train_indices, val_indices) in enumerate(skf.split(X, Y)):
         print("Training on fold " + str(index + 1) + "/10...")
 
@@ -160,14 +150,15 @@ def main(argv):
         model = None
         model = create_model()
 
-        # Debug message I guess
-        print ("Training new iteration on " + str(xtrain.shape[0])\
-              + " training samples, " + str(xval.shape[0]) + " validation samples, this may be a while...")
 
-        #end when loss func stops improving
+        print("Training new iteration on " + str(xtrain.shape[0]) \
+              + " training samples, " + str(xval.shape[0]) + " validation samples")
+
+        # end when loss func stops 'improving'
         es = EarlyStopping(monitor='val_loss', mode='min', patience=3, verbose=1)
 
-        history = model.fit([x_main, x_mhc], ytrain, epochs=EPOCHS, batch_size=96, validation_data=([x_main_val, x_mhc_val], yval), callbacks=[es])
+        history = model.fit([x_main, x_mhc], ytrain, epochs=EPOCHS, batch_size=96,
+                            validation_data=([x_main_val, x_mhc_val], yval), callbacks=[es])
         ypred = model.predict([x_main_val, x_mhc_val]).ravel()
 
         fpr_k, tpr_k, threholds_k = roc_curve(yval, ypred)
@@ -182,15 +173,13 @@ def main(argv):
             print('NEW BEST MODEL HISTORY RECORDED')
 
         all_auc.append(auc_k)
-
-
-    # model.predict()
-
-    display_data(all_auc, auc_best, fpr_keras, history_best, tpr_keras)
+    return model_best, fpr_keras, tpr_keras, auc_best
 
 
 def display_data(all_auc, auc_best, fpr_keras, history_best, tpr_keras):
     print("AUC: %.2f%% (+/- %.2f%%)" % (np.mean(all_auc), np.std(all_auc)))
+    print('mean squared error for validation data:')
+    print(np.mean(history_best.history.history['val_loss']))
     plt.figure(1)
     plt.plot([0, 1], [0, 1], 'k--')
     plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_best))
@@ -199,18 +188,49 @@ def display_data(all_auc, auc_best, fpr_keras, history_best, tpr_keras):
     plt.title('ROC curve')
     plt.legend(loc='best')
     plt.figure(1)
-    N = np.arange(0, len(history_best.history["loss"]))
+    N = np.arange(0, len(history_best.history.history["loss"]))
     plt.style.use("ggplot")
     plt.figure()
-    plt.plot(N, history_best.history["loss"], label="train_loss")
-    plt.plot(N, history_best.history["val_loss"], label="val_loss")
-    plt.plot(N, history_best.history["acc"], label="train_acc")
-    plt.plot(N, history_best.history["val_acc"], label="val_acc")
+    plt.plot(N, history_best.history.history["loss"], label="train_loss")
+    plt.plot(N, history_best.history.history["val_loss"], label="val_loss")
+    plt.plot(N, history_best.history.history["acc"], label="train_acc")
+    plt.plot(N, history_best.history.history["val_acc"], label="val_acc")
     plt.title("Training Loss and Accuracy (best model)")
     plt.xlabel("Epoch #")
     plt.ylabel("Loss/Accuracy")
     plt.legend()
     plt.show()
+
+
+def main(argv):
+    EPOCHS = 20
+    SPLITS = 10
+    # train_set_path = './data/train.tsv'
+    train_set_path = argv[1]
+    data, react = get_inputs(train_set_path)
+    X = data
+    Y = react
+
+    auc_best = 0.0
+    history_best = None
+    fpr_keras = None
+    tpr_keras = None
+    all_auc = []
+    model_best, fpr_keras, tpr_keras, auc_best = train_model(EPOCHS, SPLITS, X, Y, all_auc)
+
+    test_data_fname = argv[0]
+
+    test_data = get_inputs(test_data_fname, is_test_data=True)[0]
+    x_main_test, x_mhc_test = split_acids_mhc(test_data)
+    y_predicted_test = model_best.predict([x_main_test, x_mhc_test])
+
+    output_fname = 'out.tsv'
+    out_frame = read_data(test_data_fname)
+    out_frame.insert(2, 'Immunogenicity', y_predicted_test.ravel(), True)
+    out_frame.to_csv(output_fname, sep='\t', float_format='%.2f')
+    print('data exported to file ' + output_fname)
+
+    display_data(all_auc, auc_best, fpr_keras, model_best, tpr_keras)
 
 
 if __name__ == "__main__":
